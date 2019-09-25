5f78548cc2543beef902635824baf7f8cd605f16
ADA2029617 <ada2029617@maricopa.edugit config --global user.email ada2029617@maricopa.edugit config --global user.email ada2029617@maricopa.edu>
2019-09-25 10:38:12 -0700

    Initial Commit

---
+++ ColorMix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorMix
+//
+//  Created by Adan Partida on 9/25/19.
+//  Copyright © 2019 Adan Partida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
